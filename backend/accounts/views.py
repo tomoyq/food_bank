6afe4b1f6844a0ba106e0b613cb68db120d7731f
tomoyq <arata5.18830@gmail.com>
2025-01-23 20:38:31 +0900

    ログインapi実装#6

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class LoginView(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        #アクセスキーとリフレッシュキーをcookieに保存
+        #cookieの有効期限はtokenの期限と同じ
+        access = serializer.validated_data['access']
+        refresh = serializer.validated_data['refresh']
+
+        responce = Response(status=status.HTTP_200_OK)
+        responce.set_cookie(key='access',
+                            value=access,
+                            max_age=5 * 60,
+                            httponly=True
+                            )
+        responce.set_cookie(key='refresh',
+                            value=refresh,
+                            max_age=60 * 60 * 24,
+                            httponly=True
+                            )
+
+        return responce
